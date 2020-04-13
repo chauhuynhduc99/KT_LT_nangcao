ae1775cc82c769ee7d53260fa658937269925472
sangdtvt2 <63545348+sangdtvt2@users.noreply.github.com>
2020-04-13 20:33:18 +0700

    sang
    test3

--- Assets/Scripts/ggg.cs
+++ Assets/Scripts/ggg.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ChessMove2 : MonoBehaviour
+public class ggg : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
