e2b6b7e8a8d24fd27a3848daa05d3a85ec9b8129
chauhuynhduc99 <61837023+chauhuynhduc99@users.noreply.github.com>
2020-05-11 20:35:58 +0700

    chauhuynhduc
    done unit_move

--- Assets/Script/unit_move.cs
+++ Assets/Script/unit_move.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class unit_move : MonoBehaviour {
 	private bool mouse_down;
+	//khởi tạo mousePos
     Vector3 mousePos;
     public float minX = -3f;
     public float maxX = 4f;
@@ -13,10 +14,13 @@ public class unit_move : MonoBehaviour {
         mousePos = transform.position;
 	}
 	void OnMouseDown(){
+		//hàm thực hiện khi nhấp chuột vào quân cờ được gán
 		mouse_down = true;
 	}
 	void OnMouseUp(){
+		//hàm thực hiện khi nhả chuột ra quân cờ được gán
 		mouse_down = false;
+		//làm tròn tọa độ cho khớp vô ô cờ
 		mousePos.x = Mathf.Round(transform.position.x);
 		mousePos.y = Mathf.Round(transform.position.y);
 	}
@@ -24,9 +28,11 @@ public class unit_move : MonoBehaviour {
 	void Update () {
 	    if (Input.GetMouseButton(0) && mouse_down == true)
         {
+			//cập nhật vị trí trỏ chuột vào mousePos
             mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             mousePos = new Vector3(Mathf.Clamp(mousePos.x, minX, maxX), Mathf.Clamp(mousePos.y, minY, maxY), -2);
-        }
+		}
+		//di chuyển quân cờ tới vị trí mousePos với tốc độ 5000 (gần như ngay lập tức)
         transform.position = Vector3.Lerp(transform.position, mousePos, 5000 * Time.deltaTime);
     }
 }
