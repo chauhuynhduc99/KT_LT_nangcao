735b114e878da849e51b62876a78933b1e4bd6c8
chauhuynhduc99 <61837023+chauhuynhduc99@users.noreply.github.com>
2020-05-11 12:22:04 +0700

    chauhuynhduc
    done move

--- Assets/Script/white_king.cs
+++ Assets/Script/white_king.cs
@@ -1,57 +1,32 @@
-/*using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
-
-public class white_king : MonoBehaviour {
-	public bool mouse_down;
-	private Vector3 mouseHit; // Vị trí chuột khi nhấn
-	// Use this for initialization
-	void Start () {
-
-	}
-	public Vector3 MousePos(){
-		RaycastHit hit;
-		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-		
-		if (Physics.Raycast(ray, out hit)) {
-			return hit.point;
-		}
-		return transform.position;
-	}
-
-	void Click(){
-		mouseHit = MousePos(); // Lấy vị trí click của chuột
-		mouseHit.z = -2; // Vì game của chúng ta là 2D chính vì vậy tất cả các giá trị Z sẽ là như nhau
-		if (Input.GetMouseButton(0))
-			mouse_down = true; //Nếu chuột đang được bấm xuống
-		else
-			mouse_down = false;
-	}
-	// Update is called once per frame
-	void Update () {
-		Click();
-		if(mouse_down)
-		{
-			transform.position = mouseHit;
-		}
-	}
-}*/
 using UnityEngine;
 using System.Collections;
 
 public class white_king : MonoBehaviour {
-
-    void Update () {
-        if (Input.GetMouseButtonDown(0)) {
-            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
-            
-            RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
-            if (hit.collider != null) {
-                transform.position = mousePos2D;
-                hit.collider.attachedRigidbody.AddForce(Vector2.up);
-            }
+	private bool mouse_down;
+    Vector3 mousePos;
+    public float minX = -3f;
+    public float maxX = 4f;
+    public float minY = -3f;
+    public float maxY = 4f;
+	// Use this for initialization
+	void Start () {
+        mousePos = transform.position;
+	}
+	void OnMouseDown(){
+		mouse_down = true;
+	}
+	void OnMouseUp(){
+		mouse_down = false;
+		mousePos.x = Mathf.Round(transform.position.x);
+		mousePos.y = Mathf.Round(transform.position.y);
+	}
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetMouseButton(0) && mouse_down == true)
+        {
+            mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mousePos = new Vector3(Mathf.Clamp(mousePos.x, minX, maxX), Mathf.Clamp(mousePos.y, minY, maxY), -2);
         }
+        transform.position = Vector3.Lerp(transform.position, mousePos, 5000 * Time.deltaTime);
     }
-
 }
